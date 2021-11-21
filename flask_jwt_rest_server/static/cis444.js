4d4224742410bd2841c8552d1c4e658f93e50267
Ubuntu <ubuntu@ip-172-31-22-16.us-west-2.compute.internal>
2021-11-21 21:50:58 +0000

    latest progress

--- flask_jwt_rest_server/static/cis444.js
+++ flask_jwt_rest_server/static/cis444.js
@@ -1,4 +1,10 @@
 var jwt = null
+
+function swaplogin() {
+	$('#loginForm').toggle();
+	$('#signupForm').toggle();
+}
+
 function secure_get_with_token(endpoint, data_to_send, on_success_callback, on_fail_callback){
 	xhr = new XMLHttpRequest();
 	function setHeader(xhr) {
@@ -19,3 +25,74 @@ function secure_get_with_token(endpoint, data_to_send, on_success_callback, on_f
 		beforeSend: setHeader
 	});
 }
+
+function signup(){
+	$.post("/open_api/signup", { "username": $('#newUser').value(), "passsword":$('#newPass').value() }, function(signup){
+	console.log("success");
+
+	}, "json").fail( function(response) {
+        //this gets called if the server throws an error
+        console.log("error");
+        console.log(response);
+        });}
+		return false;
+}
+
+function login(){
+        $.post("/open_api/login", { "username": $('#user').value(), "passsword":$('#pass').value() }, function(data){
+	jwt = data.token;
+	getBooks();
+	}, "json").fail( function(response) {
+	//this gets called if the server throws an error
+	console.log("error");
+	console.log(response);
+	});
+	
+
+}
+
+function getBooks(){
+	secure_get_with_token("/secure_api/get_books", {}, function(data)
+		{console.log("got books")
+		loadBook(data.books);
+		showBook();
+	},function(err){console.log(err)}) 
+
+}
+
+function loadBook(books) {
+	var allBooks = books;
+
+	$('#bname1').html(allBooks[0][1]);
+        $('#bname2').html(allBooks[1][1]);
+        $('#bname3').html(allBooks[2][1]);
+        $('#bname4').html(allBooks[3][1]);
+        $('#bprice1').html('$' + allBooks[0][2]);
+        $('#bprice2').html('$' + allBooks[1][2]);
+        $('#bprice3').html('$' + allBooks[2][2]);
+        $('#bprice4').html('$' + allBooks[3][2]);
+
+        $('#bn1').val(allBooks[0][1]);
+        $('#bn2').val(allBooks[1][1]);
+        $('#bn3').val(allBooks[2][1]);
+        $('#bn4').val(allBooks[3][1]);
+
+        $('#bp1').val(allBooks[0][2]);
+        $('#bp2').val(allBooks[1][2]);
+        $('#bp3').val(allBooks[2][2]);
+        $('#bp4').val(allBooks[3][2]);
+}
+
+function showBook(){
+$('#loginForm').hide();
+$('#bookstore').show();
+}
+
+
+
+
+
+
+
+
+
